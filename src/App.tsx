75ea7570e9b2273a7c87fde768b8fc19a9784428
DANIEL <d4nielm7@gmail.com>
2024-08-07 15:16:00 +0200

    login otw:

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import Chats from "./components/chats/chats"
-import Detail from "./components/detail/detail"
-import Lists from "./components/lists/lists"
-
+import Chats from "./components/chats/chats";
+import Detail from "./components/detail/detail";
+import Lists from "./components/lists/lists";
+import Login from "./components/login/login"; 
+import Notification from "./components/notification/notification"; 
 const App = () => {
+
+  const user = false; 
+
   return (
     <div className='container'>
-      <Lists/> 
-      <Chats/>  
-      <Detail/> 
+      {
+        user ? (
+          <>
+            <Lists />
+            <Chats />
+            <Detail />
+          </>
+        ) : (
+          <Login />
+        )
+      }
+      <Notification/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
