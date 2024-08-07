15aba67c203a8edfe4e0fec505d42de3629f63b7
DANIEL <d4nielm7@gmail.com>
2024-08-07 16:21:50 +0200

    remove .env

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,11 +1,12 @@
-
 import { useState } from "react"
 import "./login.css"
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth,db } from "../../lib/firebase"; 
 import { setDoc ,doc} from "firebase/firestore";
+import upload from "../../lib/upload"; 
+
 const Login = () => {
   const [avatar, setAvatar] = useState<{
     file: File | null;
@@ -14,6 +15,10 @@ const Login = () => {
     file: null,
     url: "",
   });
+
+
+  const[loading,setLoading]= useState(false)
+
 
   const handleAvatar = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
@@ -24,24 +29,45 @@ const Login = () => {
     }
   };
 
-  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleLogin = async(e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
+    const formData = new FormData(e.currentTarget);
+    const formDataObj = Object.fromEntries(formData.entries());
+    const {email, password } = formDataObj as Record<string, string>;
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.log(err);
+      toast.error((err as Error).message); 
+    } finally {
+      setLoading(false);
+    }
   };
   const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
+    setLoading(true)
     const formData = new FormData(e.currentTarget);
     const formDataObj = Object.fromEntries(formData.entries());
     const { username, email, password } = formDataObj as Record<string, string>;
+
+    
     try {
       // Create a user with email and password using Firebase Auth
       const res = await createUserWithEmailAndPassword(auth, email, password);
       
-      await setDoc(doc(db,"users",res.user.uid),{
+      if (!avatar.file) {
+        throw new Error("No file selected");
+      }
+      const imgUrl = await upload(avatar.file);
+      
+      
+      await setDoc(doc(db, "users", res.user.uid), {
         username,
         email,
-        id:res.user.uid,
-        blocked:[],
+        avatar: imgUrl,
+        id: res.user.uid,
+        blocked: [],
       });
 
       await setDoc(doc(db,"userschats",res.user.uid),{
@@ -55,6 +81,8 @@ const Login = () => {
   
       // Show an error message
       toast.error(err.message);
+    } finally{
+    setLoading(false);
     }
   };
 
@@ -65,7 +93,7 @@ const Login = () => {
           <form onSubmit={handleLogin}>
             <input type ="text" placeholder="Email" name="email" />
             <input type ="password" placeholder="Password" name="password" />
-            <button>Sign In</button>
+            <button disabled={loading}>{loading ? "Loading": "Sign In"}</button>
           </form>
         </div>
         <div className="separator"></div>
@@ -79,7 +107,7 @@ const Login = () => {
             <input type ="text" placeholder="Username" name="username" />
             <input type ="text" placeholder="Email" name="email" />
             <input type ="password" placeholder="Password" name="password" />
-            <button>Sign up</button>
+            <button disabled={loading}>{loading ? "Loading": "Sign Up"}</button>
           </form>
         </div>
       </div>
